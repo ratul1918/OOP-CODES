98dbcf2a441e3ebdbe81b32d597ff352c0ead0ed
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-25 04:07:52 +0600

    Update ExceptionHandling.java

--- Exception/src/ExceptionHandling.java
+++ Exception/src/ExceptionHandling.java
@@ -1,32 +1,34 @@
+
+// Question: Write a Java program to handle ArrayIndexOutOfBoundsException, InputMismatchException, and general exceptions when inserting a value into an array based on user input.
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter array size: ");
 
-        int n=scanner.nextInt();
-        int[] myArray=new int[n];
-
+        int n = scanner.nextInt();
+        int[] myArray = new int[n];
 
         try {
             System.out.println("Enter Index positon: ");
-            int index=scanner.nextInt();
+            int index = scanner.nextInt();
             System.out.println("Enter value: ");
-            int value=scanner.nextInt();
-            myArray[index]=value;
+            int value = scanner.nextInt();
+            myArray[index] = value;
             System.out.println("Value inseeted");
 
-        }catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Arrayinndexoutofbounds occurs");
-        }catch (InputMismatchException e){
+        } catch (InputMismatchException e) {
             System.out.println("input miss match exception");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Others excption happens");
-        }
-        finally {
+        } finally {
             System.out.println("Fck");
         }
     }
 }
+
