3bbeceff7bd02a91d52b06dd6014c4922120eb1a
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-25 04:06:57 +0600

    Update Excption.java

--- Exception/src/Excption.java
+++ Exception/src/Excption.java
@@ -1,24 +1,25 @@
+
+// Question: Write a Java program that demonstrates handling of FileNotFoundException and ArithmeticException using try-catch blocks, and includes a finally block.
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class Excption {
     public static void main(String[] args) {
         try {
-            FileInputStream fis =new FileInputStream("abc.txt");
-
+            FileInputStream fis = new FileInputStream("abc.txt");
         }
         catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
 
-        try{
-            int a=100;
-            int b=0;
-            int c=a/b;
-        }catch (ArithmeticException e){
+        try {
+            int a = 100;
+            int b = 0;
+            int c = a / b;
+        } catch (ArithmeticException e) {
             System.out.println("ArithmeticException done");
-        }
-        finally {
+        } finally {
             System.out.println("Wow finally done");
         }
     }
