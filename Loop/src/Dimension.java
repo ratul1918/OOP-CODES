59bd59e83ffbf1b515751fe1a7be1d0cc8d64215
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-26 04:03:04 +0600

    Update Dimension.java

--- Loop/src/Dimension.java
+++ Loop/src/Dimension.java
@@ -1,19 +1,21 @@
 public class Dimension {
     public static void main(String[] args) {
-        String[][] cars= new String[3][3];
-        cars[0][0]="Shiiiit";
-        cars[0][1]="Shlllllit";
-        cars[0][2]="Shjjjit";
-        cars[1][1]="Shjjjjit";
-        cars[1][2]="Shiiiit";
-        cars[2][0]="Shiuuuut";
-        cars[2][1]="Shiuuut";
-        cars[2][2]="Ssssssshit";
+        String[][] cars = new String[3][3];
 
-        for (int i=0;i<cars.length;i++){
+        cars[0][0] = "CarA";
+        cars[0][1] = "CarB";
+        cars[0][2] = "CarC";
+        cars[1][0] = null;  // optional: can be set if needed
+        cars[1][1] = "CarD";
+        cars[1][2] = "CarE";
+        cars[2][0] = "CarF";
+        cars[2][1] = "CarG";
+        cars[2][2] = "CarH";
+
+        for (int i = 0; i < cars.length; i++) {
             System.out.println();
-            for (int j=0;j<cars[i].length;j++){
-                System.out.print(cars[i][j]+" ");
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.print((cars[i][j] != null ? cars[i][j] : "null") + " ");
             }
         }
     }
