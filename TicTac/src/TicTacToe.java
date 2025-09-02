96ee0150b79f8f53b743c37b7081c5906b2cdf88
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-09-03 03:50:16 +0600

    Update TicTacToe.java

--- TicTac/src/TicTacToe.java
+++ TicTac/src/TicTacToe.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class TicTacToe implements ActionListener {
 
     Random random = new Random();
-    JFrame frame = new JFrame("Tic Tac Toe");
+    JFrame frame = new JFrame("Tic Tac Toe"); 
     JPanel title_panel = new JPanel();
     JPanel button_panel = new JPanel();
     JLabel textField = new JLabel();
