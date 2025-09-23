d3fd79bb3b8df09baa9795f103bb67cdd8e84442
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-09-24 03:31:57 +0600

    Fix spacing in method call to m1()

--- Fall23/src/Main.java
+++ Fall23/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         concrete c=new concrete();
-        c.m1();
+        c.m1(); 
         c.m2();
         c.m3(); 
         c.m4();
